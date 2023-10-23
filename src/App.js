c6c42bfedaa1658cb55ca404cee6dc18060be537
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-10-23 21:42:33 +0000

    Add routes and route from react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/home';
 
 function App() {
   return (
     <div>
-      Hello there 
+     <Routes>
+        <Route path="/" element={<HomePage />} />
+     </Routes>
     </div>
   );
 }
