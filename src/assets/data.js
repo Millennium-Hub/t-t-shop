925d9d320e3721d74c1d06961bdb27d5a9cf9809
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-10-24 15:02:09 +0000

    Add basic data to project

---
+++ src/assets/data.js
@@ -0,0 +1,240 @@
+const data = {
+  categories: [
+    {
+      id: "1",
+      name: "Baby Clothing",
+      products: [
+        {
+          id: "101",
+          name: "Baby Onesie",
+          price: 15.99,
+          description:
+            "Adorable baby onesie made from soft, breathable fabric.",
+          size: "0-3 months",
+        },
+        {
+          id: "102",
+          name: "Baby Pajama Set",
+          price: 19.99,
+          description: "Cute pajama set for a good night's sleep.",
+          size: "6-12 months",
+        },
+        {
+          id: "103",
+          name: "Baby Romper",
+          price: 12.99,
+          description: "Stylish and comfortable baby romper.",
+          size: "3-6 months",
+        },
+        {
+          id: "104",
+          name: "Baby Socks (5 pairs)",
+          price: 8.99,
+          description: "Bundle of 5 soft and colorful baby socks.",
+          size: "0-6 months",
+        },
+        {
+          id: "105",
+          name: "Baby Sun Hat",
+          price: 9.99,
+          description: "Protect your baby from the sun with this adorable hat.",
+          size: "6-12 months",
+        },
+        {
+          id: "106",
+          name: "Baby Bibs (3-pack)",
+          price: 7.99,
+          description: "Set of 3 waterproof and easy-to-clean baby bibs.",
+          size: "One size",
+        },
+        {
+          id: "107",
+          name: "Baby Shoes",
+          price: 14.99,
+          description: "Soft and flexible baby shoes for little feet.",
+          size: "0-12 months",
+        },
+        {
+          id: "108",
+          name: "Baby Blanket",
+          price: 24.99,
+          description: "Warm and cozy baby blanket for snuggling.",
+          size: "One size",
+        },
+        {
+          id: "109",
+          name: "Baby Swaddle Blankets (2-pack)",
+          price: 17.99,
+          description: "Two-pack of breathable and versatile swaddle blankets.",
+          size: "One size",
+        },
+        {
+          id: "110",
+          name: "Baby T-Shirts (3-pack)",
+          price: 16.99,
+          description:
+            "Set of 3 comfortable baby T-shirts in different colors.",
+          size: "0-6 months",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Mother Care",
+      products: [
+        {
+          id: "201",
+          name: "Nursing Pillow",
+          price: 29.99,
+          description: "Supportive nursing pillow for breastfeeding moms.",
+          size: "One size",
+        },
+        {
+          id: "202",
+          name: "Maternity Dress",
+          price: 34.99,
+          description:
+            "Stylish and comfortable maternity dress for expecting mothers.",
+          size: "S, M, L, XL",
+        },
+        {
+          id: "203",
+          name: "Breast Pump",
+          price: 99.99,
+          description: "Electric breast pump for convenient milk expression.",
+          size: "One size",
+        },
+        {
+          id: "204",
+          name: "Postpartum Belly Wrap",
+          price: 19.99,
+          description: "Supportive wrap for postpartum recovery.",
+          size: "S, M, L",
+        },
+        {
+          id: "205",
+          name: "Nursing Bras (2-pack)",
+          price: 22.99,
+          description: "Two-pack of comfortable and adjustable nursing bras.",
+          size: "S, M, L, XL",
+        },
+        {
+          id: "206",
+          name: "Prenatal Vitamins",
+          price: 12.99,
+          description: "Essential vitamins for expectant mothers.",
+          size: "One size",
+        },
+        {
+          id: "207",
+          name: "Nursing Pads (30-pack)",
+          price: 8.99,
+          description: "Disposable nursing pads for leak protection.",
+          size: "One size",
+        },
+        {
+          id: "208",
+          name: "Maternity Leggings",
+          price: 27.99,
+          description: "Comfortable and stretchy leggings for moms-to-be.",
+          size: "S, M, L, XL",
+        },
+        {
+          id: "209",
+          name: "Nursing Cover",
+          price: 14.99,
+          description: "Privacy and comfort for nursing on the go.",
+          size: "One size",
+        },
+        {
+          id: "210",
+          name: "Stretch Mark Cream",
+          price: 9.99,
+          description: "Moisturizing cream to help prevent stretch marks.",
+          size: "One size",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Toys",
+      products: [
+        {
+          id: "301",
+          name: "Stuffed Bunny Toy",
+          price: 12.99,
+          description: "Soft and huggable bunny plush toy for kids.",
+          size: "One size",
+        },
+        {
+          id: "302",
+          name: "Wooden Building Blocks (50 pieces)",
+          price: 19.99,
+          description: "Set of wooden blocks for creative play.",
+          size: "One size",
+        },
+        {
+          id: "303",
+          name: "Educational Baby Mobile",
+          price: 16.99,
+          description: "Colorful and stimulating mobile for baby's crib.",
+          size: "One size",
+        },
+        {
+          id: "304",
+          name: "Kids' Art Supplies Set",
+          price: 14.99,
+          description: "Art supplies kit for budding young artists.",
+          size: "One size",
+        },
+        {
+          id: "305",
+          name: "Remote Control Car",
+          price: 24.99,
+          description: "Fun and easy-to-control remote car for kids.",
+          size: "One size",
+        },
+        {
+          id: "306",
+          name: "Puzzle Playmat",
+          price: 21.99,
+          description:
+            "Large puzzle playmat for interactive learning and play.",
+          size: "One size",
+        },
+        {
+          id: "307",
+          name: "Plush Animal Backpack",
+          price: 18.99,
+          description:
+            "Cute and practical backpack with a plush animal design.",
+          size: "One size",
+        },
+        {
+          id: "308",
+          name: "Board Books (3-pack)",
+          price: 13.99,
+          description: "Set of educational board books for young readers.",
+          size: "One size",
+        },
+        {
+          id: "309",
+          name: "Building and Construction Set",
+          price: 29.99,
+          description:
+            "Versatile building and construction set for kids' creativity.",
+          size: "One size",
+        },
+        {
+          id: "310",
+          name: "Musical Toy Instruments Set",
+          price: 26.99,
+          description:
+            "Set of musical instruments for kids to explore sound and rhythm.",
+          size: "One size",
+        },
+      ],
+    },
+  ],
+};
+export default data;
